34a9c29b4bb39d33633ea57e4dd6b619d7f15a54
SaloniRathore123 <salonirathore164@gmail.com>
2026-04-18 21:23:00 +0500

    first commit

---
+++ triangular_shapes.php
@@ -0,0 +1,52 @@
+<?php
+
+echo "<h2>Right Angle Triangle</h2>";
+$traingle_row = 6;
+$num = 6;
+ for($a = 1; $a <= $traingle_row; $a++){
+    for($b = 1; $b <= $a; $b++){
+        echo "*";
+    }
+    echo "<br>";
+ }
+echo "----------------------------------";
+echo "<h2>Inverted Minus Sign Triangle</h2>";
+for($a = $traingle_row; $a >= 1; $a--){
+    for($b = 1; $b <= $a; $b++){
+        echo "-";
+    }
+    echo "<br>";
+}
+
+echo "----------------------------------";
+echo "<h2>Number Triangle</h2>";
+ for($a = 1; $a <= $num; $a++){
+    for($b = 1; $b <= $a; $b++){
+        echo $b;
+    }
+    echo "<br>";
+ }
+
+echo "----------------------------------";
+echo "<h2>Pyramid Number Triangle</h2>";
+for($a = 1; $a <= $traingle_row; $a++){
+    for($b = $a; $b < $traingle_row; $b++){
+        echo "&nbsp;&nbsp;";
+    }
+    for($c = 1; $c <= (2 * $a - 1); $c++){
+        echo $c;
+    }
+    echo "<br>";
+}
+echo "----------------------------------";
+echo "<h2>Inverted Pyramid Triangle</h2>";
+for($a = $traingle_row; $a >= 1; $a--){
+    for($b = $traingle_row; $b > $a; $b--){
+        echo "&nbsp;&nbsp;";
+    }
+    for($c = 1; $c <= (2 * $a - 1); $c++){
+        echo "*";
+    }
+    echo "<br>";
+}
+?>
